5b38cfb6cb12bb40d614e869dd628d0e165951f5
Andrew Shi <andrewpianoluv@gmail.com>
2024-09-29 00:40:11 -0700

    fix config

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,6 @@ const isProduction = process.env.NODE_ENV === "production";
 const nextConfig = {
     output: isProduction ? "export" : "standalone",
     trailingSlash: true,
-    basePath: isProduction ? "/website" : undefined,
-    assetPrefix: isProduction ? "/website/" : undefined,
 };
 
 export default nextConfig;
